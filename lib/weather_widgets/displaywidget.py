3e0857c890619c2ca4487f025b242e08bab5046e
aimbottigerzgaming@gmail.com <brandon@pwns.me>
2024-06-01 16:31:20 -0700

    temp updata to sync new folder structure with team

---
+++ lib/weather_widgets/displaywidget.py
@@ -0,0 +1,9 @@
+class WidgetPanel:
+    def __init__(self, day_of_week, weather_type, temperature, humidity):
+        self.day_of_week = day_of_week
+        self.weather_type = weather_type
+        self.temperature = temperature
+        self.humidity = humidity
+
+
+
