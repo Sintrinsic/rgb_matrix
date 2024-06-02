d142dca2f89a44456dc2146a87bc3cca7a034e3e
sin <brandon@pwns.me>
2024-06-02 13:59:51 -0700

    adding start script

---
+++ start_server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd ~/projects/rgb_matrix
+
+# Check if the virtual environment directory exists
+if [ ! -d "venv" ]; then
+    echo "Virtual environment not found. Creating one..."
+    python3 -m venv venv
+else
+    echo "Virtual environment found."
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the required packages
+echo "Installing required packages..."
+pip install -r requirements.txt
+
+# Run the script
+echo "Running simple_display_server.py..."
+
+python simple_display_server.py
+
+# Deactivate the virtual environment
+deactivate
+
